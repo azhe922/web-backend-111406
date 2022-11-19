8c7de6ab900c37add9969b76ff73fb5a8e2e43f0
azhe <10846006@ntub.edu.tw>
2022-11-19 19:06:30 +0800

    新增查詢今日有新任務之使用者api

--- app/service/target_service.py
+++ app/service/target_service.py
@@ -100,3 +100,17 @@ def get_target_service(user_id):
 #                         return index
 #                 case TrainingPart.quadriceps:
 #                     return index
+def get_newmission_user_ids_service():
+    now = datetime.now()
+    today = now.strftime('%Y%m%d')
+    targets = Target.objects(end_date__gt=today)
+    result = []
+    if targets:
+        [__add_todo_is_notcomplete([today], target, result) for target in targets]
+    return result
+def __add_todo_is_notcomplete(check_days: list, target, result):
+    for day in check_days:
+        user_todos = target.user_todos.filter(target_date=day)
+        if user_todos and not user_todos.get().complete:
+            result.append(target.user_id)
+            return
