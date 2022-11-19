d3c6ac340da4741cf4e65dfd2601c226266e50fb
azhe <10846006@ntub.edu.tw>
2022-11-20 00:42:47 +0800

    remove private key and update path

--- app/utils/rsa_util.py
+++ app/utils/rsa_util.py
@@ -1,6 +1,5 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
-import os
 import binascii
 
 
@@ -8,10 +7,11 @@ def decrypt_token(data: bytes):
 
     # 要加密的資料（必須為 bytes）
     ciphertext = binascii.unhexlify(data)    
-    # 獲取公鑰檔案路徑
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    data_file = os.path.join(basedir, r'..\..\keys\WebBackend_PrivateKey.pem')
-    # 讀取 RSA 公鑰
+    # 獲取私鑰檔案路徑
+    # basedir = os.path.abspath(os.path.dirname(__file__))
+    # data_file = os.path.join(basedir, '/etc/secrets/WebBackend_PrivateKey.pem')
+    data_file = '/etc/secrets/WebBackend_PrivateKey.pem'
+    # 讀取 RSA 私鑰
     key = RSA.import_key(open(data_file).read())
 
     cipher = PKCS1_OAEP.new(key)
