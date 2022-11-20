1bcdd9dcba327d8b999461d5108a6970fa005b4b
azhe <10846006@ntub.edu.tw>
2022-11-20 16:26:11 +0800

    update to encrypt fcm response data

--- app/service/target_service.py
+++ app/service/target_service.py
@@ -1,5 +1,6 @@
 from app.model.target import Target
-from app.utils.backend_util import get_week
+from app.utils.backend_util import get_week, dict_to_json
+from app.utils.rsa_util import encrypt
 from datetime import datetime
 
 
@@ -11,20 +12,24 @@ def get_newmission_user_ids_service():
     now = datetime.now()
     today = now.strftime('%Y%m%d')
     targets = Target.objects(end_date__gt=today)
-    result = []
+    user_ids = []
     if targets:
-        [__add_todo_is_notcomplete([today], target, result) for target in targets]
-    return result
+        [__add_todo_is_notcomplete([today], target, user_ids) for target in targets]
+        return __convert_to_bytes_and_encrypt(user_ids).decode()
 
 def get_notcomplete_user_ids_service():
     now = datetime.now()
     today = now.strftime('%Y%m%d')
     targets = Target.objects(end_date__gt=today)
-    result = []
+    user_ids = []
     if targets:
         this_week_days = [d.strftime('%Y%m%d') for d in get_week(now) if d < now]
-        [__add_todo_is_notcomplete(this_week_days, target, result) for target in targets]            
-    return result
+        [__add_todo_is_notcomplete(this_week_days, target, user_ids) for target in targets]
+        return __convert_to_bytes_and_encrypt(user_ids).decode()
+
+def __convert_to_bytes_and_encrypt(user_ids):
+    user_ids_bytes = dict_to_json(user_ids).encode('utf-8')
+    return encrypt(user_ids_bytes)
 
 def __add_todo_is_notcomplete(check_days: list, target, result):
     for day in check_days:
--- app/utils/rsa_util.py
+++ app/utils/rsa_util.py
@@ -1,19 +1,32 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import binascii
+import os
 
 
 def decrypt_token(data: bytes):
 
-    # 要加密的資料（必須為 bytes）
-    ciphertext = binascii.unhexlify(data)    
+    # 要解密的資料（必須為 bytes）
+    ciphertext = binascii.unhexlify(data)
     # 獲取私鑰檔案路徑
-    # basedir = os.path.abspath(os.path.dirname(__file__))
-    # data_file = os.path.join(basedir, '/etc/secrets/WebBackend_PrivateKey.pem')
-    data_file = '/etc/secrets/WebBackend_PrivateKey.pem'
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    data_file = os.path.join(basedir, r'..\..\keys\WebBackend_PrivateKey.pem')
+    # data_file = '/etc/secrets/WebBackend_PrivateKey.pem'
     # 讀取 RSA 私鑰
     key = RSA.import_key(open(data_file).read())
 
     cipher = PKCS1_OAEP.new(key)
     plaintext = cipher.decrypt(ciphertext).decode('utf-8')
     return plaintext
+
+def encrypt(data: bytes):
+    # 獲取公鑰檔案路徑
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    data_file = os.path.join(basedir, r'..\..\keys\FCM_PublicKey.pem')
+
+    # 讀取 RSA 公鑰
+    key = RSA.import_key(open(data_file).read())
+
+    encryptor = PKCS1_OAEP.new(key)
+    encrypted = encryptor.encrypt(data)
+    return binascii.hexlify(encrypted)
